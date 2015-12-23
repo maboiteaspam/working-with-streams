4ea543134b0585ec7be9f37f4402118e48fc71b1
Clement Auger <clement.ager@d1m.cn>
2015-12-23 16:16:38 +0800

    add an example about pipe before carryon

---
+++ 18-pipe-returns.js
@@ -0,0 +1,45 @@
+
+/*
+ Demonstrate that A.pipe(B) returns B
+
+  Which is a matter of importance, see
+
+  if A
+  if B
+  if C
+                              write() ▶ ▼
+  then A             A|_ -> transform() ▶ ▼
+       .pipe(B)        B|_ -> transform() ▶ ▼
+         .pipe(C)        C|_ -> transform() ▶ ▼
+
+ It is much different than
+
+ if A
+ if B
+ if C
+                             write() ▶ ▼
+ then A             A|_ -> transform() ▼ ▶
+ A.pipe(B)          B|_ -> transform() ▼ ▶
+ A.pipe(C)          C|_ -> transform() ▼ ▶
+
+
+
+ */
+var demo = function () {
+
+
+  var streamA = through2();
+  var streamB = through2();
+
+  console.log(
+    streamA.pipe(streamB)===streamB
+  )
+};
+
+// boiler plate stuff for the demo
+process.env['DEBUG'] = 'stream';
+
+var debug = require('debug')('stream')
+var through2 = require('through2');
+
+demo();
