1130400d27c5de161833b7b0ffefe5f231a705ba
Clement Auger <clement.ager@d1m.cn>
2015-12-23 17:21:40 +0800

    carryon example for demonstration

--- 18-pipe-returns.js
+++ 18-pipe-returns.js
@@ -4,23 +4,21 @@
 
   Which is a matter of importance, see
 
-  if A
-  if B
-  if C
+  if A, B, C
 
   then A             A|_ -> transform() ▶ ▼
        .pipe(B)        B|_ -> transform() ▶ ▼
          .pipe(C)        C|_ -> transform() ▶ ▼
           .write()        ▶  ▶ applies to C
+
+
  It is much different than
 
- if A
- if B
- if C
+ if A, B, C
 
- then A             A|_ -> transform() ▼ ▶
- A.pipe(B)          B|_ -> transform() ▼ ▶
- A.pipe(C)          C|_ -> transform() ▼ ▶
+ then A             A|_ -> transform() ▶ ▼
+ A.pipe(B)          B|_ -> transform() ▶ ▼
+ A.pipe(C)          C|_ -> transform() ▶ ▼
  A.write()            ▶  ▶ applies to A
 
 
